610508a20b3616ca7429325e448e58ff8ce1a4db
ohjongchan <jongchan_oh@tmax.co.kr>
2023-03-06 16:57:21 +0900

    fix: remember me 제거

--- src/pages/users/Login.tsx
+++ src/pages/users/Login.tsx
@@ -192,19 +192,6 @@ export const Login = () => {
                         <FormError errorMessage={errors.password?.message} />
                       )}
                     </div>
-                    <div>
-                      <label className="inline-flex items-center cursor-pointer">
-                        <input
-                          id="customCheckLogin"
-                          type="checkbox"
-                          className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
-                          style={{ transition: "all .15s ease" }}
-                        />
-                        <span className="ml-2 text-sm font-semibold text-gray-700">
-                          Remember me
-                        </span>
-                      </label>
-                    </div>
 
                     <Button
                       canClick={isValid}
