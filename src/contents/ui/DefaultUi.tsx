cafd6cf986710451c5472d5bad1a30862bbf0df5
ohjongchan <jongchan_oh@tmax.co.kr>
2023-03-10 13:55:01 +0900

    feat: default ui 가이드 버튼 구성

--- src/contents/ui/DefaultUi.tsx
+++ src/contents/ui/DefaultUi.tsx
@@ -1,8 +1,32 @@
 export const DefaultUi = () => {
   return (
     <div className="absolute bottom-4 left-4">
-      <div className="w-32">
-        <img alt="arrow" src="/img/arrow.png" />
+      <div className="flex flex-col items-center w-32">
+        <div className="text-white p-2">이동</div>
+        <div className="mb-2 text-center">
+          <kbd className="keyboard-button">W</kbd>
+        </div>
+        <div className="flex flex-row text-center mb-2 gap-1">
+          <kbd className="keyboard-button">A</kbd>
+          <kbd className="keyboard-button">S</kbd>
+          <kbd className="keyboard-button">D</kbd>
+        </div>
+        <div className="text-white p-2">점프</div>
+        <div className="text-center mb-2">
+          <kbd className="keyboard-button">Space</kbd>
+        </div>
+        <div className="text-white p-2">시야 조정</div>
+        <div className="flex flex-row mb-2 gap-1">
+          <kbd className="keyboard-button">Click</kbd>
+          <span className="text-white">+</span>
+          <kbd className="keyboard-button">Move</kbd>
+        </div>
+        <div className="text-white p-2">채팅</div>
+        <div className="flex flex-row gap-1">
+          <kbd className="keyboard-button">ESC</kbd>
+          <span className="text-white">+</span>
+          <kbd className="keyboard-button">Input</kbd>
+        </div>
       </div>
     </div>
   );
